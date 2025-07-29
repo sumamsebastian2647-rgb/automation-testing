4fe1ed12ec38e37757d4d2f636c041e026001fde
Sumam Sebastian <sumam@cloudemy.com.au>
2025-07-29 05:48:26 +0000

    config.js edited online with Bitbucket

--- Course_Forum_with_cohort/config.js
+++ Course_Forum_with_cohort/config.js
@@ -10,7 +10,7 @@ module.exports = {
         course: 'music',
         Trainer:'Kerstin Tony',
         courseFullName: 'CUA60520 - Advanced Diploma',
-        topicTitle: 'test_automate_13',
+        topicTitle: 'test_automate_14',
         topicDescription: 'this is to test forum',
         cohort: 'Music-Cohort1 - Australia'
     }
